4e6341d11e9ac628179f6b466fc8946e6643a3f5
Jactry Zeng <jzeng@codeweavers.com>
2014-10-05 22:27:12 +0800

    Add some trace.

--- hellodll_v1.c
+++ hellodll_v1.c
@@ -16,7 +16,10 @@ BOOL WINAPI DllMain(HINSTANCE hinst, DWORD reason, LPVOID reserved)
     case DLL_PROCESS_ATTACH:
       instance = hinst;
       DisableThreadLibraryCalls(hinst);
+      printf("hellodll_v1.dll attach\n");
       break;
+    case DLL_PROCESS_DETACH:
+      printf("hellodll_v1.dll detach\n");
   }
   return TRUE;
 }
