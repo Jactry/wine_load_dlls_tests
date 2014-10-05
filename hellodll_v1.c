f4090caf338936facb358eb208e22b94385ccd27
Jactry Zeng <jzeng@codeweavers.com>
2014-10-05 21:35:33 +0800

    Reimplement dlls by DllMain.

--- hellodll_v1.c
+++ hellodll_v1.c
@@ -1,7 +1,22 @@
 /* hellodll_v1.c */
+#include <windows.h>
 #include <stdio.h>
 
 __declspec(dllexport) void __cdecl get_version()
 {
   printf("hellodll_v1\n");
+}
+
+static HINSTANCE instance;
+
+BOOL WINAPI DllMain(HINSTANCE hinst, DWORD reason, LPVOID reserved)
+{
+  switch (reason)
+  {
+    case DLL_PROCESS_ATTACH:
+      instance = hinst;
+      DisableThreadLibraryCalls(hinst);
+      break;
+  }
+  return TRUE;
 }
--- hellodll_v2.c
+++ hellodll_v2.c
@@ -1,7 +1,22 @@
 /* hellodll_v2.c */
+#include <windows.h>
 #include <stdio.h>
 
 __declspec(dllexport) void __cdecl get_version()
 {
   printf("hellodll_v2\n");
+}
+
+static HINSTANCE instance;
+
+BOOL WINAPI DllMain(HINSTANCE hinst, DWORD reason, LPVOID reserved)
+{
+  switch (reason)
+  {
+    case DLL_PROCESS_ATTACH:
+      instance = hinst;
+      DisableThreadLibraryCalls(hinst);
+      break;
+  }
+  return TRUE;
 }
